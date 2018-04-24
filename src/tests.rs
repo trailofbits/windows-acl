df389b25124e08c5eaa8a7e12e974821b5239412
Andy Ying <andy@trailofbits.com>
2018-04-24 17:53:49 -0400

    comments

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,10 @@
+// powershell.exe -ExecutionPolicy bypass -File setup_tests.ps1
+
+/*
+TODO: Implement PowerShell scripts to setup a nest of folders and files for testing
+TODO: Run the setup script before each test
+*/
+
 #[test]
 fn it_works() {
     assert_eq!(2 + 2, 4);
